8f5a7e56985249fa0f2b4e08b65f48edddfbb33b
phaitoon srinil <phaitoon@buu.ac.th>
2020-07-23 11:16:52 +0700

    remove password

--- .node-red/settings.js
+++ .node-red/settings.js
@@ -117,14 +117,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    adminAuth: {
-       type: "credentials",
-       users: [{
-           username: "admin",
-           password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-           permissions: "*"
-       }]
-    },
+    // adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: "admin",
+    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+    //        permissions: "*"
+    //    }]
+    // },
 
     // adminAuth: {
     //     type: 'credentials',
